bd04b652f2cda68f084683eda56715a2cbb83e12
andreygal <andreygalper@gmail.com>
2019-03-27 14:47:46 -0400

    php script for the form section

--- index.php
+++ index.php
@@ -336,12 +336,17 @@
             <div class="row" id="form">
                 <form method="post" action="mailer-new.php" class="contact-form">
                     <?php
-                    <div class="form-messages success">
-                            Thank you! Your message has been sent.
-                    </div> 
-                    <div class="form-messages error">
-                            Something went wrong. Please try again. 
-                    </div>
+                    if($_GET['success'] == 1) { 
+                        echo "<div class=\"form-messages success\">
+                                Thank you! Your message has been sent.
+                             </div>"
+                    }    
+                    
+                    if($_GET['success'] == -1) { 
+                        echo "<div class=\"form-messages success\">
+                                Something went wrong. Please try again. 
+                              </div>"
+                    }
                     ?>
                     <div class="row">
                        <div class="col span-1-of-3">
